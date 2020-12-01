a72b70358a2988bfec9bcc4e89fa3878294e8de2
ri-heme <72443078+ri-heme@users.noreply.github.com>
2020-12-01 15:17:41 +0100

    Update scripts
    Changes to "2. TFA":
    
    - stores relaxation table for anaylsis
    
    Changes to "3. Plotting":
    
    - plots total dG
    - plots dG relaxation analysis
    - when plotting reaction dG, only the reactions that are different from WT are selected and plotted
    
    Changes to "utils.py":
    
    - add subroutine to relax LC instead of dGo
    - add option to relax LC/dGo when using the optimize function
    - add function to retrieve a bound change table from the relaxation table produced by pytfa's relaxation algo

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from cobra.exceptions import Infeasible
 from cobra.io import load_json_model
 from pytfa import ThermoModel
 from pytfa.io import load_thermoDB, read_lexicon, annotate_from_lexicon, read_compartment_data, apply_compartment_data
 from pytfa.optim import DeltaG, LogConcentration
 from pytfa.optim.relaxation import relax_dgo
+from pytfa.utils.numerics import BIGM
 
 CENTRAL_CARBON_METABOLISM = [ 'Citric Acid Cycle', 'Pentose Phosphate Pathway', 'Glycolysis/Gluconeogenesis', 'Glyoxylate Metabolism', 'Oxidative Phosphorylation', 'Pyruvate Metabolism' ]
 
@@ -39,7 +41,7 @@ def create_model(name, cobra_path, thermo_data, lexicon, compartment_data, bioma
 
 def adjust_model(tmodel, lc_bounds, rxn_bounds):
     """
-    Adjusts the log concentration bounds of a TFA model and knocks out the specified reaction.
+    Adjusts the log concentration and flux bounds of a TFA model.
     """
     # patch ATPM (non-growth associated maintenance estimated by Orth et al for P/O ratio = 1.375)
     tmodel.parent.reactions.ATPM.lower_bound = 3.15
@@ -54,23 +56,17 @@ def adjust_model(tmodel, lc_bounds, rxn_bounds):
             tmodel.parent.reactions.get_by_id(rxn_id).bounds = lb, ub
             tmodel.reactions.get_by_id(rxn_id).bounds = lb, ub
 
-def optimize(tmodel, frisky_reactions):
+def optimize(tmodel, relax = 'dgo', frisky_vars = []):
     """
-    Solves the LP problem and returns the (relaxed) model.
+    Relaxes and solves the LP problem. Returns the relaxed model and a table with the magnitude of the bound violations.
     """
-    # Solve TFA
-    try:
-        tfa_solution = tmodel.optimize()
-        relax = tfa_solution.objective_value < 0.1
-    # Infeasible solutions raise this exception
-    except AttributeError:
-        relax = True
-    # Relax dG constraints and solve again if need be
-    if relax:
-        relaxed_model, _, _ = relax_dgo( tmodel, reactions_to_ignore = frisky_reactions )
+    if relax == 'dgo':
+        relaxed_model, _, relax_table = relax_dgo( tmodel, reactions_to_ignore = frisky_vars )
         relaxed_model.optimize()
-        return relaxed_model
-    return tmodel
+        return relaxed_model, get_dgo_bound_change( relaxed_model, relax_table )
+    elif relax == 'lc':
+        return relax_lc( tmodel, metabolites_to_ignore = frisky_vars )
+    raise ValueError('Only standard Gibbs energy (`dgo`) or log concentration (`lc`) relaxation allowed.')
 
 def get_flux(tmodel):
     """
@@ -96,3 +92,68 @@ def get_log_concentration(tmodel):
     index = pd.Index( tmodel.log_concentration.list_attr('id'), name = 'metabolite' )
     compartment = [ tmodel.compartments[ tmodel.metabolites.get_by_id( met_id ).compartment ]['name'] for met_id in index ]
     return pd.DataFrame( { 'log_concentration': tmodel.get_primal(LogConcentration).values, 'compartment': compartment }, index = index )
+
+def get_dgo_bound_change(tmodel, relax_table):
+    """
+    Returns a 2-column DataFrame containing the reaction bound changes and the subsystem their reaction belongs to.
+    """
+    bound_change = relax_table['ub_change'] - relax_table['lb_change']
+    relax_table.index.name = 'reaction'
+    return pd.DataFrame( {
+        'bound_change': bound_change.values,
+        'subsystem': tmodel.reactions.query( lambda rxn_id : rxn_id in relax_table.index, 'id' ).list_attr( 'subsystem' ) },
+        index = relax_table.index )
+
+def relax_lc(tmodel, relax_obj_type = 0, metabolites_to_ignore = [], destructive = True):
+    """
+    Uses the Gurobi subroutines to relax the log concentration bounds. Returns a table with the magnitude of the bound violations.
+    """
+    if tmodel.solver.interface.__name__ != 'optlang.gurobi_interface':
+        raise ModuleNotFoundError('Requires Gurobi.')
+
+    # copy Gurobi model
+    grb_model = tmodel.solver.problem.copy()
+    
+    # get the Gurobi log concentration variables
+    lc_vars = [ grb_model.getVarByName( var.name ) for var in tmodel.log_concentration if  var.id not in metabolites_to_ignore ]
+    vars_penalties = [1] * len(lc_vars)
+
+    # perform relaxation of variable bounds
+    relax_obj = grb_model.feasRelax(relaxobjtype = relax_obj_type,
+                                    minrelax = True,
+                                    vars = lc_vars,
+                                    lbpen = vars_penalties,
+                                    ubpen = vars_penalties,
+                                    constrs = None,
+                                    rhspen = None)
+    
+    # check if relaxation was successful
+    if relax_obj < 0:
+        raise Infeasible('Failed to create the feasibility relaxation!')
+        
+    grb_model.optimize()
+    
+    # transfer the bound changes from Gurobi Model to ThermoModel
+    rows = []
+    metabolites = tmodel.log_concentration.list_attr('id')
+    for met_id in metabolites:
+        # get the auxiliary LB/UB change variables
+        lb_change = -grb_model.getVarByName('ArtL_LC_' + met_id).X
+        ub_change = grb_model.getVarByName('ArtU_LC_' + met_id).X
+        
+        # check if the auxiliary variables are not 0 (indicating a bound change)
+        if lb_change < 0 or ub_change > 0:
+            
+            # set new bounds on the original log concentration variable
+            if destructive:
+                var = tmodel.log_concentration.get_by_id(met_id).variable
+                var.set_bounds( lb = max( -BIGM, min( BIGM, var.lb + lb_change ) ),
+                                ub = max( -BIGM, min( BIGM, var.ub + ub_change ) ) )
+            
+            rows.append( { 'metabolite': met_id, 'bound_change': lb_change + ub_change,
+                'compartment': tmodel.compartments[ tmodel.metabolites.get_by_id( met_id ).compartment ]['name'] } )
+    
+    tmodel.optimize()
+    
+    # return table of relaxed variables
+    return pd.DataFrame.from_records( rows, index = 'metabolite' )
